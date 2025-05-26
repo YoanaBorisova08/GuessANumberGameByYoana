a28871622d52d8ba8783fb320af850ceebf93cf2
Yoana Borisova <yoanasb08@gmail.com>
2025-05-26 13:01:19 +0300

    Update guess_a_number.py

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,9 +1,12 @@
 import random
 
 computer_number = random.randint(1, 100)
+range_number = 100
+round = 1
+tries = 10
 
 while True:
-    player_input = input("Guess the number (1-100): ")
+    player_input = input(f"Round {round}! {\n}You have {tries} tries! {\n}Guess the number (1-{range_number}): ")
     if not player_input.isdigit():
         print("Invalid input. Try again...")
         continue
@@ -12,8 +15,27 @@ while True:
 
     if player_number == computer_number:
         print("You guessed right!")
+        play_again = input("Type [yes] if you want to go to the NEXT ROUND. Else type [no].")
+        if play_again == "yes":
+            round+=1
+            tries -=1
+            range_number+=100
+            continue
         break
     elif player_number < computer_number:
         print("Too Low!")
+        tries-=1
     else:
         print("Too High!")
+        tries-=1
+
+    if tries==1:
+        print("One last try. Be careful!")
+    elif tries==0:
+        print("You lost! {\n} Type [yes] if you want to PLAY AGAIN. Else type [no].")
+        if play_again == "yes":
+            round = 1
+            tries = 10
+            range_number = 100
+        else:
+            break
