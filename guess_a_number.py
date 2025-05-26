fed37da2e2adfc580314d27306ad2ae3a9d5318a
Yoana Borisova <yoanasb08@gmail.com>
2025-05-26 13:13:21 +0300

    Corrected some mistakes

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,45 +1,52 @@
 import random
 
+def printRed(skk): print("\033[91m {}\033[00m" .format(skk))
+def printGreen(skk): print("\033[92m {}\033[00m" .format(skk))
+def printYellow(skk): print("\033[93m {}\033[00m" .format(skk))
+def printBlue(skk): print("\033[94m {}\033[00m" .format(skk))
+
 computer_number = random.randint(1, 100)
 range_number = 100
 round_number = 1
 tries = 10
 
-print(f"Round {round_number}! You have {tries} tries!")
+printBlue(f"Round {round_number}! You have {tries} tries!")
 
 while True:
     player_input = input(f"Guess the number (1-{range_number}): ")
     if not player_input.isdigit():
-        print("Invalid input. Try again...")
+        printRed("Invalid input. Try again...")
         continue
 
     player_number = int(player_input)
 
     if player_number == computer_number:
-        print("You guessed right!")
+        printGreen("You guessed right!")
         play_again = input("Type [yes] if you want to go to the NEXT ROUND. Else type [no].")
         if play_again == "yes":
             round_number+=1
             tries = 10
             range_number+=100
-            print(f"Round {round_number}! You have {tries} tries!")
+            computer_number = random.randint(1, range_number)
+            printBlue(f"Round {round_number}! You have {tries} tries!")
             continue
         break
     elif player_number < computer_number:
-        print("Too Low!")
+        printYellow("Too Low!")
         tries-=1
     else:
-        print("Too High!")
+        printYellow("Too High!")
         tries-=1
 
     if tries==1:
-        print("One last try. Be careful!")
+        printRed("One last try. Be careful!")
     elif tries==0:
         play_again = input("You lost! Type [yes] if you want to PLAY AGAIN. Else type [no].")
         if play_again == "yes":
             round_number = 1
             tries = 10
             range_number = 100
-            print(f"Round {round_number}! You have {tries} tries!")
+            computer_number = random.randint(1, range_number)
+            printBlue(f"Round {round_number}! You have {tries} tries!")
         else:
             break
